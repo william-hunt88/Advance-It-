329abd88d6b49217a5b78d6aaef1d7f8566c5a58
w.hunt <wfh2d88@gmail.com>
2021-03-31 18:29:35 -0500

    for presentation

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -2,11 +2,8 @@ const background = document.querySelector(".background")
 const blur = document.querySelector(".blur")
 
 
-background.setAttribute("style", "background: #A27AD8");
 background.setAttribute("style", "background-image: none");
+background.setAttribute("style", "background: #A27AD8");
 blur.setAttribute("style", "background: rgba(237, 255, 224, 0.550)")
-background.setAttribute("style", "width: 100vw")
-background.setAttribute("style", "height: 100vh")
-blur.setAttribute("style", "width: 100vw")
-blur.setAttribute("style", "height: 100vh")
+
 
